50729858f7441d8de44dd2c3c9472ecefe31a4f2
Martin Hogg <martinhogg@Martins-MacBook-Pro.local>
2017-08-15 18:16:55 +0100

    Tidied up code

--- tools/scipy-examples.py
+++ tools/scipy-examples.py
@@ -1,13 +1,11 @@
 # Gaussian (a.k.a. normal) distribution
 import numpy as np
 import matplotlib.pyplot as plt
-
 from scipy.stats import norm
 
 # Probability Density Function (PDF)
 r = np.random.randn(1000)
 p = norm.pdf(r)
-
 plt.scatter(r,p)
 plt.title("Probability Density Function (PDF)")
 plt.xlabel("r")
@@ -17,7 +15,6 @@ plt.show()
 # Log Probability Density Function (PDF)
 r = np.random.randn(1000)
 p = norm.logpdf(r)
-
 plt.scatter(r,p)
 plt.title("Log Probability Density Function (PDF)")
 plt.xlabel("r")
@@ -27,7 +24,6 @@ plt.show()
 # Cumulative Probability Density Function (PDF)
 r = np.random.randn(1000)
 p = norm.cdf(r)
-
 plt.scatter(r,p)
 plt.title("Cumulative Probability Density Function (PDF)")
 plt.xlabel("r")
@@ -37,7 +33,6 @@ plt.show()
 # Log Cumulative Probability Density Function (PDF)
 r = np.random.randn(1000)
 p = norm.logcdf(r)
-
 plt.scatter(r,p)
 plt.title("Log Cumulative Probability Density Function (PDF)")
 plt.xlabel("r")
@@ -47,7 +42,6 @@ plt.show()
 # Sampling from Gaussian Distribution (1D)
 # Standard Deviation=1, Mean=0
 r = np.random.randn(1000)
-
 plt.hist(r, bins=100)
 plt.title("Sampling from Gaussian Distribution (1D) - Standard Deviation=1, Mean=0")
 plt.xlabel("r")
@@ -57,7 +51,6 @@ plt.show()
 # Sampling from Gaussian Distribution (1D)
 # Standard Deviation=10, Mean=5
 r= 10*np.random.randn(1000) + 5
-
 plt.hist(r, bins=100)
 plt.title("Sampling from Gaussian Distribution (1D) - Standard Deviation=10, Mean=5")
 plt.xlabel("r")
@@ -67,16 +60,8 @@ plt.show()
 # Sample from Gaussian Distribution (2D)
 # Standard Deviation=1, Mean=0
 r = np.random.randn(1000, 2)
-
 plt.scatter(r[:, 0], r[:, 1])
 plt.title("Sampling from Gaussian Distribution (2D)")
 plt.xlabel("r[:, 0]")
 plt.ylabel("r[:. 1]")
 plt.show()
-
-# Sample for General Multi-variate Normal Distribution (Full Covariance Matrix)
-# cov 
-# mu 
-# r = mvn.rvs(mean=mu, cov=cov, size=1000)
-# plt.scatter(r[:, 0], r[:, 1])
-# plt.show()
