7e2743e188d6dc9b99199aa1de15b9a047bec6ec
Martin Hogg <martinhogg@Martins-MacBook-Pro.local>
2017-08-15 16:02:34 +0100

    Added titles and axes to charts

--- tools/matplotlib-examples.py
+++ tools/matplotlib-examples.py
@@ -1,16 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Create some x & y co-ordiantes to plot
+# Create some x & y coordinates to plot
 x = np.linspace(0, 10, 11)
 y = np.sin(x)
 
 # Points
 plt.scatter(x,y)
+plt.title("Scatter")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.show()
 
 # Points joined by lines
 plt.plot(x, y)
+plt.title("Plot")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.show()
 
 # Create a gaussian distribution to plot
@@ -18,5 +24,8 @@ g = np.random.randn(1000)
 
 # Histogram
 plt.hist(g)
+plt.title("Histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
 plt.show()
 
