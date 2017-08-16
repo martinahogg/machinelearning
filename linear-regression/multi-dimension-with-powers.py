7ed8afaeee3bc71df8c6ba4a43d120cd4a1ad344
Martin Hogg <martinhogg@Martins-MacBook-Pro.local>
2017-08-16 14:10:23 +0100

    Few tweaks to sample

---
+++ linear-regression/multi-dimension-with-powers.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+# Create some training samples.
+
+Xraw = np.array([ 2, 3, 9, 12])
+
+# Our original input sample are 2, 3, 9 & 12. We choose
+# to include each value to the power of 0, 1 & 2 in
+# separate columns of our input.
+#
+# By preparing our input values in this way, the algorithm 
+# can derive a polynomial (i.e. a curved plane or hyper-plane).
+X = np.array([
+[math.pow(Xraw[0], 0), math.pow(Xraw[0], 1), math.pow(Xraw[0],2)],
+[math.pow(Xraw[1], 0), math.pow(Xraw[1], 1), math.pow(Xraw[1],2)],
+[math.pow(Xraw[2], 0), math.pow(Xraw[2], 1), math.pow(Xraw[2],2)],
+[math.pow(Xraw[3], 0), math.pow(Xraw[3], 1), math.pow(Xraw[3],2)] ])
+
+Y = np.array([ 41,81, 615, 1071])
+
+# Derive the weights that give the lowest error.
+w = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, Y))
+
+# Calcualte the predicted y values.
+Yhat = np.dot(X, w)
+
+# Calculate a score of the accuracy of our predictions.
+d1 = Y - Yhat
+d2 = Y - Y.mean()
+rsquared = 1 - d1.dot(d1) / d2.dot(d2)
+
+# Plot the training points and predicted line of best fit
+plt.scatter(Xraw, Y)
+plt.plot(Xraw, Yhat)
+plt.title("Multi Dimension Linear Regression")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
