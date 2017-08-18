b006df851b32fb0f68233ccf9e7f49d6cc9f3053
Martin Hogg <martin.hogg@gmail.com>
2017-08-18 13:29:47 +0100

    Updated examples to add a cubed term and more data points

--- linear-regression/multi-dimension-polynomial.py
+++ linear-regression/multi-dimension-polynomial.py
@@ -4,7 +4,7 @@ import math
 
 # Create some training samples.
 
-Xraw = np.array([ 2, 3, 9, 12])
+Xraw = np.array([1, 2, 3, 4, 5, 6])
 
 # Our original input sample are 2, 3, 9 & 12. We choose
 # to include each value to the power of 0, 1 & 2 in
@@ -13,12 +13,14 @@ Xraw = np.array([ 2, 3, 9, 12])
 # By preparing our input values in this way, the algorithm 
 # can derive a polynomial (i.e. a curved plane or hyper-plane).
 X = np.array([
-[math.pow(Xraw[0], 0), math.pow(Xraw[0], 1), math.pow(Xraw[0],2)],
-[math.pow(Xraw[1], 0), math.pow(Xraw[1], 1), math.pow(Xraw[1],2)],
-[math.pow(Xraw[2], 0), math.pow(Xraw[2], 1), math.pow(Xraw[2],2)],
-[math.pow(Xraw[3], 0), math.pow(Xraw[3], 1), math.pow(Xraw[3],2)] ])
+  [math.pow(Xraw[0], 0), math.pow(Xraw[0], 1), math.pow(Xraw[0],2), math.pow(Xraw[0],3)],
+  [math.pow(Xraw[1], 0), math.pow(Xraw[1], 1), math.pow(Xraw[1],2), math.pow(Xraw[1],3)],
+  [math.pow(Xraw[2], 0), math.pow(Xraw[2], 1), math.pow(Xraw[2],2), math.pow(Xraw[2],3)],
+  [math.pow(Xraw[3], 0), math.pow(Xraw[3], 1), math.pow(Xraw[3],2), math.pow(Xraw[3],3)],
+  [math.pow(Xraw[4], 0), math.pow(Xraw[4], 1), math.pow(Xraw[4],2), math.pow(Xraw[4],3)],
+  [math.pow(Xraw[5], 0), math.pow(Xraw[5], 1), math.pow(Xraw[5],2), math.pow(Xraw[5],3)] ])
 
-Y = np.array([ 41,81, 615, 1071])
+Y = np.array([27, 80, 181, 342, 575, 892])
 
 # Derive the weights that give the lowest error.
 w = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, Y))
